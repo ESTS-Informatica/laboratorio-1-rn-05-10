cf6845087dc79fe48e4002e3d67b8138fe1889cc
miguelcandido13 <202200131@estudantes.ips.pt>
2024-03-13 09:49:19 +0000

    Nível 2
    Implementação do InputReader na classe WordGuessingGame e o método play com os métodos privados necessários.

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -9,7 +9,8 @@ public class WordGuessingGame
     private String hiddenWord;
     private String guessedWord;
     private int numberOfTries;
-
+    private InputReader reader;
+    
     /**
      * Constructor for objects of class WordGuessingGame
      */
@@ -18,6 +19,7 @@ public class WordGuessingGame
         this.hiddenWord = "abc";
         this.guessedWord = "___";
         this.numberOfTries = 0;
+        this.reader = new InputReader();
     }
 
     public String getHiddenWord(){
@@ -34,5 +36,41 @@ public class WordGuessingGame
     
     public void showGuessedWord(){
         System.out.println("Guessed Word:  " + this.guessedWord);
+    }
+    
+    private void showWelcome(){
+        System.out.println("Bem vindo, bora jogar?!");
+    }
+
+    private boolean guess(char letter){
+        boolean found = false;
+        StringBuilder newGuessedWord = new StringBuilder(guessedWord);
+        for(int i=0; i < hiddenWord.length(); i++){
+            if(hiddenWord.charAt(i) == letter && guessedWord.charAt(i) == '_'){
+                newGuessedWord.setCharAt(i, letter);
+                found = true;
+            }
+        }
+        if(found){
+            guessedWord = newGuessedWord.toString();
+        }
+        return found;
+    }
+    
+    private void showResult(){
+        System.out.println("Conseguiu resolver em " + this.numberOfTries + " tentativas!");
+    }
+    
+    public void play(){
+        showWelcome();
+        while(!guessedWord.equals(hiddenWord)){
+            char guess = reader.getChar("Guess a letter:  ");
+            this.numberOfTries++;
+            if(!guess(guess)){
+                System.out.println("Incorrect guess! Try again!");
+            }
+            showGuessedWord();
+        }
+        showResult();
     }
 }
